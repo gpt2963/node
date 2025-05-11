8a6806c370fc3737cc6444b29d27b2ce6f9a4738
Shushant Shrestha <gpt2963@gmail.com>
2025-05-11 20:18:28 +0545

    app test content

---
+++ test/app.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET Endpoints', function () {
+  it('should return welcome message at root', function (done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .expect(200, '{ "response": "Congratulation on Your First deployment" }', done);
+  });
+
+  it('should return Hello World on /hello', function (done) {
+    request(app)
+      .get('/hello')
+      .expect('Content-Type', /json/)
+      .expect(200, '{ "response": "Hello World" }', done);
+  });
+
+  it('should return readiness message on /ready', function (done) {
+    request(app)
+      .get('/ready')
+      .expect('Content-Type', /json/)
+      .expect(200, '{ "response": " Great!, It works!" }', done);
+  });
+});
